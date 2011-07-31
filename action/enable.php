75f5267916a1c8d4a4768c2c562a0aa743d0c72f
Mark Liffiton <liffiton@gmail.com>
2011-07-30 21:11:06 -0500

    From MathJax docs: HTML5 makes it illegal to have content for a script with a src attribute.

--- action/enable.php
+++ action/enable.php
@@ -22,13 +22,20 @@ class action_plugin_mathjax_enable extends DokuWiki_Action_Plugin {
        $controller->register_hook('TPL_METAHEADER_OUTPUT', 'BEFORE', $this, 'handle_tpl_metaheader_output');
     }
 
-    // Add a <script> tag to the headers to load MathJax
+    // Add two <script> tags to the headers:
+    //  One to load MathJax and one to configure it
+    //  See http://www.mathjax.org/docs/1.1/configuration.html#using-in-line-configuration-options
     public function handle_tpl_metaheader_output(Doku_Event &$event, $param) {
+        $event->data['script'][] = array(
+			'type'    => 'text/x-mathjax-config',
+			'charset' => 'utf-8',
+			'_data'   => $this->getConf('config'),
+		);
         $event->data['script'][] = array(
 			'type'    => 'text/javascript',
 			'charset' => 'utf-8',
-			'_data'   => $this->getConf('config'),
 			'src'     => $this->getConf('url')
+			'_data'   => '',
 		);
     }
 
