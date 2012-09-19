eb9f9cd4d624c37b797c5a0b59efb784901955b8
Mark Liffiton <liffiton@gmail.com>
2012-09-19 12:26:20 -0500

    Update URLs to point to latest Mathjax documentation.

--- lang/en/settings.php
+++ lang/en/settings.php
@@ -1,5 +1,5 @@
 <?php
 
 $lang['url'] = 'The URL from which MathJax will be loaded.';
-$lang['config'] = 'MathJax configuration.  See http://www.mathjax.org/docs/1.1/configuration.html';
+$lang['config'] = 'MathJax configuration.  See http://www.mathjax.org/docs/2.0/options/';
 
