d74cb97eeaa1fb27d036be4711676fcc5cb01ef6
Mark Liffiton <liffiton@gmail.com>
2012-12-08 22:55:14 -0600

    Configuration explanation updates.

--- lang/en/settings.php
+++ lang/en/settings.php
@@ -1,6 +1,6 @@
 <?php
 
 $lang['url'] = 'The URL from which MathJax will be loaded.';
-$lang['config'] = 'MathJax configuration.  See http://docs.mathjax.org/en/latest/options/';
-$lang['configfile'] = '(Optional) One or more files containing MathJax configuration commands.  Specify paths relative to dokuwiki root, and separate multiple files with semicolons.';
+$lang['config'] = '(Optional) MathJax configuration; javascript code executed when MathJax loads.  See http://docs.mathjax.org/en/latest/options/';
+$lang['configfile'] = '(Optional) One or more files containing MathJax configuration commands.  Specify paths relative to the dokuwiki installation directory (e.g. conf/mathjax.js or data/pages/mathjaxconf.txt), and separate multiple files with semicolons.';
 
