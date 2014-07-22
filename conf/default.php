afc515d4250708caf95a000c6d905aa3e85db774
Mark Liffiton <liffiton@gmail.com>
2014-07-22 10:42:51 -0500

    Changed default URL to new CDN URL w/ auto-https.  Fixes #13.

--- conf/default.php
+++ conf/default.php
@@ -5,7 +5,7 @@
  * @author Mark Liffiton <liffiton@gmail.com>
  */
 
-$conf['url'] = 'https://c328740.ssl.cf1.rackcdn.com/mathjax/latest/MathJax.js?config=TeX-AMS_HTML';
+$conf['url'] = '//cdn.mathjax.org/mathjax/latest/MathJax.js?config=TeX-AMS_HTML';
 $conf['config'] = 'MathJax.Hub.Config({
     tex2jax: {
         inlineMath: [ ["$","$"], ["\\\\(","\\\\)"] ],
