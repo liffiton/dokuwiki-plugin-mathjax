58cc79e52b8b8fe4d95fc3e5aa460d27b98f8fa4
Gerrit Uitslag <klapinklapin@gmail.com>
2014-11-19 14:55:00 +0100

    phpdocs add latex output mode Latex output support based at https://www.dokuwiki.org/plugin:latexit  - performs some replacements  - output mode for latex

--- syntax/protecttex.php
+++ syntax/protecttex.php
@@ -9,12 +9,9 @@
 // must be run within Dokuwiki
 if (!defined('DOKU_INC')) die();
 
-if (!defined('DOKU_LF')) define('DOKU_LF', "\n");
-if (!defined('DOKU_TAB')) define('DOKU_TAB', "\t");
-if (!defined('DOKU_PLUGIN')) define('DOKU_PLUGIN',DOKU_INC.'lib/plugins/');
-
-require_once DOKU_PLUGIN.'syntax.php';
-
+/**
+ * Class syntax_plugin_mathjax_protecttex
+ */
 class syntax_plugin_mathjax_protecttex extends DokuWiki_Syntax_Plugin {
     # We need to grab any math before dokuwiki tries to parse it.
     # Once it's 'claimed' by this plugin (type: protected), it won't be altered.
@@ -43,15 +40,31 @@ class syntax_plugin_mathjax_protecttex extends DokuWiki_Syntax_Plugin {
         "gather\*",
     );
 
+    /**
+     * Syntax Type
+     *
+     * Needs to return one of the mode types defined in $PARSER_MODES in parser.php
+     *
+     * @return string
+     */
     public function getType() {
         return 'protected';
     }
 
+    /**
+     * Sort for applying this mode
+     *
+     * @return int
+     */
     public function getSort() {
         return 65;
     }
 
-    // regexp patterns adapted from jsMath plugin: http://www.dokuwiki.org/plugin:jsmath
+    /**
+     * regexp patterns adapted from jsMath plugin: http://www.dokuwiki.org/plugin:jsmath
+     *
+     * @param string $mode
+     */
     public function connectTo($mode) {
         $this->Lexer->addEntryPattern('(?<!\\\\)\$(?=[^\$][^\r\n]*?\$)',$mode,'plugin_mathjax_protecttex');
         $this->Lexer->addEntryPattern('\$\$(?=.*?\$\$)',$mode,'plugin_mathjax_protecttex');
@@ -70,17 +83,57 @@ class syntax_plugin_mathjax_protecttex extends DokuWiki_Syntax_Plugin {
         }
     }
 
-    public function handle($match, $state, $pos, &$handler){
+    /**
+     * Handler to prepare matched data for the rendering process
+     *
+     * This function can only pass data to render() via its return value - render()
+     * may be not be run during the object's current life.
+     *
+     * Usually you should only need the $match param.
+     *
+     * @param   string       $match   The text matched by the patterns
+     * @param   int          $state   The lexer state for the match
+     * @param   int          $pos     The character position of the matched text
+     * @param   Doku_Handler $handler The Doku_Handler object
+     * @return  array Return an array with all data you want to use in render
+     */
+    public function handle($match, $state, $pos, Doku_Handler $handler){
+
+        $match = str_replace('<=>', '\Leftrightarrow', $match);
+        $match = str_replace('<->', '\leftrightarrow', $match);
+        $match = str_replace('->', '\rightarrow', $match);
+        $match = str_replace('<-', '\leftarrow', $match);
+        $match = str_replace('=>', '\Rightarrow', $match);
+        $match = str_replace('<=', '\Leftarrow', $match);
+        $match = str_replace('...', '\ldots', $match);
+        $match = str_replace('−', '-', $match);
+
         // Just pass it through...
         return $match;
     }
 
+    /**
+     * Handles the actual output creation.
+     *
+     * @param   $mode     string        output format being rendered
+     * @param   $renderer Doku_Renderer the current renderer object
+     * @param   $data     array         data created by handler()
+     * @return  boolean                 rendered correctly?
+     */
     public function render($mode, &$renderer, $data) {
-        if($mode != 'xhtml') return false;
+        if($mode != 'xhtml') {
+            /** @var Doku_Renderer_xhtml $renderer */
 
-        // Just pass it through, but escape xml entities...
-        $renderer->doc .= $renderer->_xmlEntities($data);
+            // Just pass it through, but escape xml entities...
+            $renderer->doc .= $renderer->_xmlEntities($data);
+            return true;
+        }
 
+        if ($mode == 'latex') {
+
+            $renderer->doc .= $data;
+            return true;
+        }
         return true;
     }
 }
