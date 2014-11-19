d9fe6da94f8ae6ff4b9a00c7ab59f45fbf3ba497
Gerrit Uitslag <klapinklapin@gmail.com>
2014-11-19 17:01:20 +0100

    phpdocs action component

--- action/enable.php
+++ action/enable.php
@@ -9,23 +9,27 @@
 // must be run within Dokuwiki
 if (!defined('DOKU_INC')) die();
 
-if (!defined('DOKU_LF')) define('DOKU_LF', "\n");
-if (!defined('DOKU_TAB')) define('DOKU_TAB', "\t");
-if (!defined('DOKU_PLUGIN')) define('DOKU_PLUGIN',DOKU_INC.'lib/plugins/');
-
-require_once DOKU_PLUGIN.'action.php';
-
+/**
+ * Add scripts via an event handler
+ */
 class action_plugin_mathjax_enable extends DokuWiki_Action_Plugin {
 
-    // Register our handler for the TPL_METAHEADER_OUTPUT event
+    /**
+     * Registers our handler for the TPL_METAHEADER_OUTPUT event
+     */
     public function register(Doku_Event_Handler &$controller) {
        $controller->register_hook('TPL_METAHEADER_OUTPUT', 'BEFORE', $this, 'handle_tpl_metaheader_output');
     }
 
-    // Add <script> blocks to the headers:
-    //  One to load MathJax and one to configure it
-    //  Also add one block per configfile, if any are specified
-    //  See http://docs.mathjax.org/en/latest/configuration.html#using-in-line-configuration-options
+    /**
+     * Add <script> blocks to the headers:
+     *   - One to load MathJax and one to configure it
+     *   - Also add one block per configfile, if any are specified
+     * See http://docs.mathjax.org/en/latest/configuration.html#using-in-line-configuration-options
+     *
+     * @param Doku_Event $event
+     * @param            $param
+     */
     public function handle_tpl_metaheader_output(Doku_Event &$event, $param) {
         // Create main config block
         $event->data['script'][] = array(
